29de32c795ced68f065376d686e3d754e05b2a79
hongghe <364930855@qq.com>
2018-08-18 19:43:53 +0800

    Initial commit

---
+++ src/test/java/com/hongghe/protobufdemo/ProtobufDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hongghe.protobufdemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProtobufDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
